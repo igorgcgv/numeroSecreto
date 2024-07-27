0aa46b5991b63c88c89630baa77b3bc4b7bb9c69
unknown <Igor Galeno>
2024-07-27 12:02:17 -0300

    Inserido as condicionais para mostrar as tentativas feitas até o acerto

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 alert('Bem vindo ao jogo do número secreto'); // exibe uma caixa de dialogo "Alerta"
-let numeroSecreto = 9; //Para criação de Variaveis utiliza a clausula "let" semelhante a linguagem M
-console.log(numeroSecreto) //console.log é utilizado para visualizar o resultado no navegador na aba de dev
-let chute 
+let numeroSecreto = 10; //Para criação de Variaveis utiliza a clausula "let" semelhante a linguagem M
+console.log(numeroSecreto); //console.log é utilizado para visualizar o resultado no navegador na aba de dev
+let chute; 
+let tentativas = 1;
 
 
 
@@ -9,15 +10,23 @@ let chute
 while (chute!=numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 30'); // 'prompt' é utilizado para imput
     if(chute == numeroSecreto) { //Sintaxe para o operador "if"      
-        alert(`Acertou! o número secreto é : ${numeroSecreto}`); // Semelhante ao f-string do python f'Algumas coisa {variavel}'
+        break; 
     } else {
         if(chute>numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         }
-            else
+            else {
                 alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;    
                
     }
+}
+
+if (tentativas > 1){
+    alert(`Acertou! o número secreto é : ${numeroSecreto} com ${tentativas} tentativas`); // Semelhante ao f-string do python f'Algumas coisa {variavel}'
+} else{
+    alert(`Acertou! o número secreto é : ${numeroSecreto} com ${tentativas} tentativa`); // Semelhante ao f-string do python f'Algumas coisa {variavel}'
 }
 
 
